d0d4ffce81cee454ec1174bdfad8c69525a3ae2e
flakster <carlos.santamaria@gmail.com>
2020-11-30 19:18:37 -0500

    Add the code to return the requested data

--- app/controllers/heatmap_controller.rb
+++ app/controllers/heatmap_controller.rb
@@ -1,5 +1,13 @@
 class HeatmapController < ApplicationController
   def index
-    render plain: 'hello'
+    @result =[]
+    pairs = {}
+    drivers = Response.select(:driver_name).distinct
+    drivers.each do |driver|
+      scores = Response.joins(:employee).group(:department).having(:driver_name == driver).average(:score)  
+      pairs[driver.driver_name] = scores
+    end 
+    @result << pairs
+    render json:@result
   end
 end
