a155e6bacdd7602a2c6902fbabd4e037e9459c60
flakster <carlos.santamaria@gmail.com>
2020-12-21 15:17:27 -0500

    Fix error in the query

--- app/controllers/heatmap_controller.rb
+++ app/controllers/heatmap_controller.rb
@@ -1,10 +1,11 @@
 class HeatmapController < ApplicationController
   def index
     @result =[]
-    pairs = {}
     drivers = Response.select(:driver_name).distinct
     drivers.each do |driver|
-      scores = Response.joins(:employee).group(:department).having(:driver_name == driver).average(:score)  
+      #scores = Response.joins(:employee).group(:department).having(:driver_name == driver).average(:score)  
+      scores = Response.joins(:employee).group(:department).where('driver_name = ?', driver.driver_name).average(:score)  
+      pairs = {}
       pairs['driver'] = driver.driver_name
       pairs['scores'] = scores
       @result << pairs
