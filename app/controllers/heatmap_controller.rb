cf7a3a6346e782b8aa43b682625ff201e5d010a7
flakster <carlos.santamaria@gmail.com>
2020-12-21 15:01:55 -0500

    fix the output format

--- app/controllers/heatmap_controller.rb
+++ app/controllers/heatmap_controller.rb
@@ -5,9 +5,10 @@ class HeatmapController < ApplicationController
     drivers = Response.select(:driver_name).distinct
     drivers.each do |driver|
       scores = Response.joins(:employee).group(:department).having(:driver_name == driver).average(:score)  
-      pairs[driver.driver_name] = scores
+      pairs['driver'] = driver.driver_name
+      pairs['scores'] = scores
+      @result << pairs
     end 
-    @result << pairs
     render json:@result
   end
 end
